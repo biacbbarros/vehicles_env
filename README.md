7dcdfbcd2d413b1402fa61ab2697c7b812d027c2
biacbbarros <biacbbarros@gmail.com>
2024-06-07 16:14:08 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vehicles_env
+
+#O projeto é um aplicativo que visualiza a análise de dados de vendas de carros. As funcionalidades são a exibição de um histograma, que mostra a distribuição da quilometragem dos carros, e um gráfico de dispersão, que relaciona a quilometragem com os preços dos veículos.
---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+# Caminho para o arquivo
+arq_caminho = 'C:/Users/bia/OneDrive/Documentos/01.Curso Análise de Dados/Sprint 5/vehicles_env/vehicles_us.csv'
+
+car_data = pd.read_csv(arq_caminho)  # lendo os dados
+
+st.header('Vendas de carros')  # criando o titulo
+
+hist_button = st.button('Criar histograma')  # criando botão para o histograma
+if hist_button:  # se o botão for clicado
+    # escrever uma mensagem
+    st.write(
+        'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+# criar um histograma
+fig_hist = px.histogram(car_data, x="odometer")
+
+# exibir um gráfico Plotly interativo
+st.plotly_chart(fig_hist, use_container_width=True)
+
+
+# criando botão para o grafico de disperão
+scatter_button = st.button('Criar gráfico de dispersão')
+if scatter_button:  # se o botão for clicado
+    # escrever uma mensagem
+    st.write(
+        'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+# criar um grafico de dispersão
+fig_scatter = px.scatter(car_data, x="odometer", y="price")
+
+# exibir um gráfico Plotly interativo
+st.plotly_chart(fig_scatter, use_container_width=True)
